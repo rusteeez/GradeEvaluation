2c011a9ab6279f0e6bf235a640c8dfea09d5d848
Jade McClain <jademcclain04@gmail.com>
2024-09-02 11:13:15 -0700

    add while loop

--- Grade.java
+++ Grade.java
@@ -2,54 +2,61 @@ import java.util.Scanner;
 public class Grade {
 
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        String grade = new String();
+        int i = 1;
+        while(i == 1){
+            Scanner scnr = new Scanner(System.in);
+            String grade = new String();
+            //int i = 1;
 
-        //ask user for the type of assignment
-        System.out.println("What is the type of the assessment? Type 1 for midterm, type 2 for project, and type 3 for final.");
-        int type = scnr.nextInt();
+            //ask user for the type of assignment
+            System.out.println("What is the type of the assessment? Type 1 for midterm, type 2 for project, and type 3 for final.");
+            int type = scnr.nextInt();
 
-        //ask user for their score
-        System.out.println("Enter the score:");
-        int score = scnr.nextInt();
+            //ask user for their score
+            System.out.println("Enter the score:");
+            int score = scnr.nextInt();
 
         
-        if(type == 1) {// midterm
-            grade = findGrade(score, 90,90,65,65);
-        }
-        else if(type == 2) {//project
+            if(type == 1) {// midterm
+                grade = findGrade(score, 90,90,65,65);
+            }
+            else if(type == 2) {//project
             grade = findGrade(score, 85, 85, 50, 50);
-        }
-        else if(type == 3){//final exam
-            grade = findGrade(score, 80, 80, 60, 60);
-        }
-        else{
-            System.out.println("Invalid Input.");
-        }
+            }
+            else if(type == 3){//final exam
+                grade = findGrade(score, 80, 80, 60, 60);
+            }
+            else{
+                System.out.println("Invalid Input.");
+            }
         
-        //Print out the grade for the submitted assessment
-        System.out.printf("Grade for Assessment is %s", grade);
+            //Print out the grade for the submitted assessment
+            System.out.printf("Grade for Assessment is %s", grade);
+            System.out.println("Did you wish to evaluate another assessment? Type 1 for yes, 2 for no.");
+            i = scnr.nextInt();
+            //scnr.close();
+            }
+        
 
-        scnr.close();
+        //scnr.close();
 
     }
-
     //method will take in the ranges and generate the grade based on it.
     public static String findGrade(int score, int lowerLevelExcellent, int upperLevelGood, int lowerLevelGood, int upperLevelPoor){
 
         String grade = new String();
         //figure out the grade
         if(score >= lowerLevelExcellent){
-            grade = "Excellent";
+            grade = "Excellent. ";
         }
         else if(score < upperLevelGood && score >= lowerLevelGood){
-            grade = "Good";
+            grade = "Good. ";
         }
         else if(score < upperLevelPoor){
-            grade = "Poor";
+            grade = "Poor. ";
         }
         else{
-            grade = "Invalid Score";
+            grade = "Invalid Score. ";
         }
         
         return grade;
